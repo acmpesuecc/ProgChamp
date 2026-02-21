67c31a4dae48b6ad6f72198b5bb4df3053331abf
Pratham <pratkavi2006@gmail.com>
2026-02-21 17:32:54 +0530

    tags route added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,18 @@
 import { Hono } from "hono";
+<<<<<<< Updated upstream
+=======
+import { cors } from "hono/cors";
+import auth from "./routes/auth";
+import profile from "./routes/profile";
+import tagsRoute from './routes/tags';
+
+>>>>>>> Stashed changes
 
 const app = new Hono();
 
 app.get("/", (c) => c.text("OK"));
+
+app.route("/tags", tagsRoute);
 
 export default app;
 
